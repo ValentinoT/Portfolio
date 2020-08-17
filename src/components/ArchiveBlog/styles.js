7e14703dfd7d69c1a7431b99d6ac7751eb6048d4
ValentinoT <danieltorresc13@gmail.com>
2020-08-16 21:32:58 -0500

    Archive Blog Responsive

--- src/components/ArchiveBlog/styles.js
+++ src/components/ArchiveBlog/styles.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components'
+import { device } from '../styles/Generals'
 
 export const ContainerBlog = styled.section`
   width: 100%;
   margin: 0 20px 0 120px;
+  @media ${device.tablet}{
+    margin: 16px 16px 86px 16px;
+  }
 `
 
 export const GridPost = styled.div`
@@ -11,6 +15,12 @@ export const GridPost = styled.div`
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 40px;
   margin-top: 20px;
+  @media ${device.laptop}{
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media ${device.tablet}{
+    grid-template-columns: repeat(1, 1fr);
+  }
 `
 
 export const LastPostContainer = styled.div`
@@ -18,6 +28,9 @@ export const LastPostContainer = styled.div`
   grid-column-end: 3;
   && a{
     text-decoration: none; 
+  }
+  @media ${device.tablet}{
+    grid-column-end: 2;
   }
 `
 export const PostContainer = styled.div`
@@ -34,6 +47,15 @@ export const ContainerImage = styled.div`
   overflow: hidden;
   && .gatsby-image-wrapper{
     height: 280px;
+  }
+  @media ${device.mobileL}{
+    height: 250px;
+  }
+  @media ${device.mobileM}{
+    height: 225px;
+  }
+  @media ${device.mobileS}{
+    height: 200px;
   }
 `
 export const HeaderCardPost = styled.div`
@@ -52,6 +74,9 @@ export const TitlePost = styled.h4`
   flex-grow: 1;
   margin-right: 10px;
   text-decoration: none;
+  @media ${device.tablet}{
+    font-size: 16px;
+  }
 `
 
 export const DatePost = styled.time`
@@ -70,4 +95,7 @@ export const ExtractPost = styled.p`
   transition: all 0.15s linear;
   line-height: 20px;
   text-align: justify;
+  @media ${device.mobileL}{
+    text-align: left;
+  }
 `
